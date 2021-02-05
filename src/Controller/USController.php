715b6632fa5b94fe69ab2337cda93aea270335f3
Paul Massias <pmassias001@iutbayonne.univ-pau.fr>
2021-02-05 17:34:58 +0100

    Mise en place du répertoire et préparation au tri par module

--- src/Controller/USController.php
+++ src/Controller/USController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Semestre;
+use App\Entity\Module;
+use App\Repository\SemestreRepository;
 
 class USController extends AbstractController
 {
@@ -19,12 +22,28 @@ class USController extends AbstractController
     }
 	
 	/**
-	 * @Route("/repertoire", name="us_repertoires")
+	 * @Route("/repertoires", name="us_repertoires")
 	 */
-	 public function repertoires(): Response
+	 public function repertoires()
 	 {
-		 $repositorySemstre = $this->getDoctrine()->getRepository(Semestre::class);
-		 $Semestres = $repositorySemstre->findAll();
-		 return $this->render('us/repertoires.html.twig', ['semestre' => '$Semestre', ]);
+		 $SemestreRepository = $this->getDoctrine()->getRepository(Semestre::class);
+		 $semestres = $SemestreRepository->findAll();
+		 return $this->render('us/repertoires.html.twig', ['semestres' => $semestres]);
 	 }
+	 
+	 
+	 
+	 
+	
+	 
+	 
+	/**
+	 * @Route("/Module/{id}", name="us_postModule")
+	 */
+	 public function triPostParModule($id)
+	 {
+		 $module= $this->getDoctrine()->getRepository(Module::class)->find($id);
+		 return $this->render('us/Module.html.twig', ['module' => $module,]);
+	 }
+		 
 }
