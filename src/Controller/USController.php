bf45971461844ca551565713de8fb113e4aa39e8
Altiroh <blandinewen@gmail.com>
2021-02-05 19:21:34 +0100

    Suppression des modifications de la branche inferieur

--- src/Controller/USController.php
+++ src/Controller/USController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class USController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="us_accueil")
+     * @Route("/us/accueil", name="us_accueil")
      */
     public function accueil(): Response
     {
