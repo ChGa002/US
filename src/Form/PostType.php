8a20c2e16d4fe3db1c58e80cdb5a94775fe29f60
Chiara Gandolfi <cgandolfi@iutbayonne.univ-pau.fr>
2021-02-09 22:16:04 +0100

    Formulaire basic non fonctionnel + creation RessourceType

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,25 +2,49 @@
 
 namespace App\Form;
 
+use App\Repository\ModuleRepository;
 use App\Entity\Post;
+use App\Entity\Module;
+use App\Entity\MotCle;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\ChoiceList\ArrayChoiceList;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Form\RessourceType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('datePubli')
-            ->add('description')
-            ->add('signale')
-            ->add('emplacementPhoto')
-            ->add('motsCles')
-            ->add('modules')
-            ->add('createur')
-        ;
+            ->add('titre', TextType::class)
+            ->add('description', TextareaType::class, ['attr'=> ['style'=>'resize:none', 'rows'=>'2']])
+            ->add('emplacementPhoto', UrlType::class)
+            ->add('motsCles', TextType::class, ['attr' => ['class' => 'js-user-autocomplete']])
+            ->add('modules', EntityType::class, array('class' => Module::class,
+                'choice_label' => function(Module $module)
+                {return $module->getSigle() ;}, 
+                'choice_attr' => function(Module $module)
+                {return array($module->getSigle() => $module->getNom()) ;},
+                'expanded' => true,
+                'multiple' => true
+                ))
+             ->add('ressources', CollectionType::class, [
+                        'entry_type' => RessourceType::class,
+                        'entry_options' => ['label' => false],
+                        'allow_add' => true,
+                        'allow_delete' => true,
+                        'by_reference' => false
+                ])
+            ;
+        
     }
 
     public function configureOptions(OptionsResolver $resolver)
---
+++ src/Form/RessourceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ressource;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+class RessourceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+			->add('nom', TextType::class, [ 'attr' => ['placeholder' => 'Nom du fichier...'] ])
+            ->add('typeDeFichier', TextType::class, [ 'attr' => ['placeholder' => 'Extension du fichier... (jpeg, pdf, odt)'] ] )
+            ->add('emplacement', UrlType::class, [ 'label' => 'Lien ressource', 'attr' => ['placeholder' => 'Lien vers la ressource...'] ]);         
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ressource::class,
+        ]);
+    }
+}
