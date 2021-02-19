97fe01cccf7ad7727e994500ad4afa868ea4dc55
Altiroh <blandinewen@gmail.com>
2021-02-20 00:38:21 +0100

    Finalisation cgmt pseudo / mdp

--- src/Controller/ParametreController.php
+++ src/Controller/ParametreController.php
@@ -7,23 +7,24 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ParametreController extends AbstractController
 {
      /**
-     * @Route("/us/parametre", name="us_parametre")
+     * @Route("/us/parametre/parametre", name="us_parametre")
      */
     public function param(): Response
     {
-
-        return $this->render('/us/parametre.html.twig', [
-            'controller_name' => 'USController',
-        ]);
+        return $this->render('/parametre/parametre.html.twig');
     }
      /**
-     * @Route("/us/parametre/changerPseudo", name="us_changePseudo")
+     * @Route("/us/parametre/changementPseudo", name="us_changePseudo")
      */
     public function changerPseudo(Request $request, EntityManagerInterface $manager): Response
     {
@@ -38,13 +39,107 @@ class ParametreController extends AbstractController
             $manager->persist($user);
             $manager->flush();
 
-            return $this->redirectToRoute('us_parametre');        
+            return $this->redirectToRoute('us_parametre'); 
+            $request->getSession()->getFlashBag()->add('notice', "Votre pseudo a été mis à jour.");             
         }
-        return $this->render('/us/parametre.html.twig', 
+        return $this->render('/parametre/changementPseudo.html.twig', 
         [ 
            'user' => $user,
            'pseudoForm'=>$pseudoForm->createView(),
         ]);
     }
+      /**
+     * @Route("/us/parametre/changementMotDePasse", name="us_changeMDP")
+     */
+    public function changerMotDePasse(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $manager): Response
+    {
+        $user  = $this->getUser();
+
+        $mdpForm = $this->createFormBuilder($user)
+        ->add('motDePasse', PasswordType::class, [
+            'attr'=>['placeholder'=>'Nouveau Mot de Passe'],
+            /*'mapped' => false,*/
+            'label' => false,
+            'constraints' => [
+                new NotBlank(['message' => 'Entrer le mot de passe.', ]),
+                new Length([
+                    'min' => 8,
+                    'minMessage' => 'Votre mot de passe doit faire 8 caractères',
+                    // max length allowed by Symfony for security reasons
+                    'max' => 4096,
+                     ])
+                    ]
+                ])
+        ->getForm();
+        $mdpForm->handleRequest($request);
+
+        if ($mdpForm->isSubmitted() && $mdpForm->isValid()) {
+            $user->setMotDePasse(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $mdpForm->get('motDePasse')->getData()
+                )
+            );
+            $manager->persist($user);
+            $manager->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', "Votre mot de passe a été mis à jour.");      
+            return $this->redirectToRoute('us_parametre');  
+            
+        }
+        return $this->render('/parametre/changementMotDePasse.html.twig', 
+        [ 
+           'user' => $user,
+           'mdpForm'=>$mdpForm->createView(),
+        ]);
+    }
+    /**
+     * @Route("/us/parametre/supprimerCompte", name="us_supprimerCompte")
+     */
+    public function supprimerCompte(Request $request)
+    {
+        $active = 'delete';
+        $user = $this->getUser();
+         
+        $suppForm = $this->createFormBuilder()
+                                    ->add('motDePasse', PasswordType::class, [
+                                        'attr'=>['placeholder'=>'Nouveau Mot de Passe'],
+                                        /*'mapped' => false,*/
+                                        'label' => false,
+                                        'constraints' => [
+                                            new NotBlank(['message' => 'Entrer le mot de passe.', ]),
+                                            new Length([
+                                                'min' => 8,
+                                                'minMessage' => 'Votre mot de passe doit faire 8 caractères',
+                                                // max length allowed by Symfony for security reasons
+                                                'max' => 4096,
+                                                 ])
+                                                ]
+                                            ])
+                                    ->getForm();
+         
+        if($suppForm->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()
+                       ->getManager()
+                       ;
+                        
+            $em->remove($user);
+            $em->flush();
+             
+            //$this->get('security.context')->setToken(null);
+            $this->get('request')->getSession()->invalidate();
+             
+            $request->getSession()->getFlashBag()->add('notice', "Votre compte a bien été supprimé.");
+         
+            return $this->redirect($this->generateUrl('app_login'));
+        }
+         
+        return $this->render('/parametre/supprimerCompte.html.twig', [
+            'suppForm' => $suppForm->createView(),
+            'active' => $active
+        ]);
+    }
+
 
 }
