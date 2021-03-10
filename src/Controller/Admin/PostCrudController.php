c50cc27cb38dbfe0d2c9664dd25d206c6dd0bcb9
Altiroh <blandinewen@gmail.com>
2021-03-10 22:06:31 +0100

    Avancement : Liaison Post aux autres entités en cours

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostCrudController extends AbstractCrudController
@@ -12,14 +16,20 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
             TextEditorField::new('description'),
+            TextField::new('createur'),
+            BooleanField::new('signale')->hideOnForm(),
+            TextEditorField::new('emplacementPhoto')->hideOnIndex(),
+            CollectionField::new('motCles')->hideOnIndex(),
+            CollectionField::new('ressources')->hideOnIndex(),
+            CollectionField::new('modules')->hideOnIndex(),
         ];
     }
-    */
+    
 }
--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -3,9 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -26,10 +32,15 @@ class UtilisateurCrudController extends AbstractCrudController
             TextField::new('prenom'),
             EmailField::new('mail'),
             TextEditorField::new('description')->hideOnForm(),
-            DateTimeField::new('derniereConnexion')->hideOnForm(),
+            DateTimeField::new('derniereConnexion')->onlyOnDetail(),
+            BooleanField::new('valide')->hideOnForm(),
+            ArrayField::new('roles')->hideOnForm(),
         ];
     }
 
-    // public function configureForms
+    // public function configureActions(Actions $actions): Actions{
+    //     return $actions ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    // }
+
     
 }
